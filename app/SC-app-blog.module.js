9783e9acb82e91ae11a575ab828b2e4d1e7ad454
samueljmurray <samueljmurray@gmail.com>
2015-03-12 12:25:14 +0000

    initial commit

---
+++ app/SC-app-blog.module.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name SC-app-blog
+ * @description
+ *
+ * Provides the app with the ability to display blog content and features
+ */
+angular
+  .module('SC-app-blog');
